82eff1eea00a95dd1ebfb70605accb07e78287bb
Michael Ameteku <michaelameteku@gmail.com>
2021-10-26 23:09:06 -0400

    Create calender page structure

--- thought-cloud/src/App.js
+++ thought-cloud/src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import { LoginPage } from './login_page/login-page.js';
 import {DashBoard} from "./account_page/account_page.js"
+import { MainClassPage } from './main_class_page/main_class_page';
 
 function App() {
   return (
     <div className="App">
-    <DashBoard/>
+    <MainClassPage/>
     </div>
   );
 }
