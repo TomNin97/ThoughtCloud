634e53e5f9a190e35335f599120b8501eea8b62c
Michael Ameteku <michaelameteku@gmail.com>
2021-11-27 21:20:29 -0500

    Add files

---
+++ thought-cloud/src/main_class_page/calender-page/calender-page.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import CourseRequests from '../../backend-request/course-request';
+import { AddEventModal, CalenderComponent, CustomButton } from '../../shared-components/shared-components';
+import "../calender-page/calender-page.css";
+
+export class CalenderPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            course: props.course,
+            courseRequests : new CourseRequests()
+        }
+    }
+
+
+    render() {
+        return (
+
+            <div className="page-wrapper">
+
+                <AddEventModal courseRequests={this.state.courseRequests} course={this.state.course} />
+                <div className="subtitle-section">
+                    <h2>Calender</h2>
+                    <CustomButton title="Add Event" />
+                </div>
+                <div className="calender-wrapper">
+                    <CalenderComponent />
+                </div>
+            </div>
+        );
+    }
+}
